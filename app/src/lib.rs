703ee775dfba44d04b3880c61bd3c1e73a0cce3e
Nakayama Daiju <42.daiju@gmail.com>
2023-10-22 22:54:16 +0900

    refactor: gl系処理をbackendとして切り出し

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,16 +1,18 @@
-use bytemuck::{Pod, Zeroable};
-use glow::{Context, HasContext, WebBufferKey};
-use memoffset::offset_of;
 use rand::{Rng, SeedableRng};
 use rand_pcg::Mcg128Xsl64;
 use wasm_bindgen::prelude::*;
-use web_sys::{console, WebGl2RenderingContext, WebGlUniformLocation};
+use web_sys::{console, WebGl2RenderingContext};
 
 use color::RGBA;
 use rmath::{Deg, Matrix};
 
-use crate::{key::KeyManager, player::Player};
+use crate::{
+    backend::{Backend, Vertex},
+    key::KeyManager,
+    player::Player,
+};
 
+mod backend;
 mod key;
 mod player;
 
@@ -21,187 +23,6 @@ fn wasm_error(s: String) -> JsValue {
 #[allow(dead_code)]
 fn log(s: String) {
     console::log_1(&s.into());
-}
-
-pub struct Backend {
-    gl: Context,
-    vbo: WebBufferKey,
-    ebo: WebBufferKey,
-    model_matrix_location: WebGlUniformLocation,
-    lorentz_matrix_location: WebGlUniformLocation,
-    view_perspective_location: WebGlUniformLocation,
-    triangle_count: usize,
-}
-
-#[derive(Debug, Clone, Copy, Zeroable, Pod)]
-#[repr(C)]
-pub struct Vertex {
-    local_position: [f32; 3],
-    world_position: [f32; 3],
-    scale: [f32; 3],
-    color: RGBA,
-}
-
-impl Backend {
-    pub fn new(webgl2: WebGl2RenderingContext, indices: &[[u32; 3]]) -> Result<Self, String> {
-        let gl = Context::from_webgl2_context(webgl2);
-        unsafe {
-            gl.enable(glow::DEPTH_TEST);
-            gl.clear_color(0.9, 0.9, 0.9, 1.0);
-            gl.clear(glow::COLOR_BUFFER_BIT | glow::DEPTH_BUFFER_BIT);
-
-            let program = gl.create_program()?;
-            let fragment_shader_source = include_str!("assets/fragment_shader.glsl");
-            let vertex_shader = gl.create_shader(glow::VERTEX_SHADER)?;
-            gl.shader_source(vertex_shader, include_str!("assets/vertex_shader.glsl"));
-            gl.compile_shader(vertex_shader);
-            if !gl.get_shader_compile_status(vertex_shader) {
-                return Err(gl.get_shader_info_log(vertex_shader));
-            }
-            gl.attach_shader(program, vertex_shader);
-            let fragment_shader = gl.create_shader(glow::FRAGMENT_SHADER)?;
-            gl.shader_source(fragment_shader, fragment_shader_source);
-            gl.compile_shader(fragment_shader);
-            if !gl.get_shader_compile_status(fragment_shader) {
-                return Err(gl.get_shader_info_log(fragment_shader));
-            }
-            gl.attach_shader(program, fragment_shader);
-            gl.link_program(program);
-            if !gl.get_program_link_status(program) {
-                return Err(gl.get_program_info_log(program));
-            }
-            gl.detach_shader(program, vertex_shader);
-            gl.delete_shader(vertex_shader);
-            gl.detach_shader(program, fragment_shader);
-            gl.delete_shader(fragment_shader);
-            gl.use_program(Some(program));
-
-            let vbo = gl.create_buffer()?;
-            gl.bind_buffer(glow::ARRAY_BUFFER, Some(vbo));
-            let ebo = gl.create_buffer()?;
-            gl.bind_buffer(glow::ELEMENT_ARRAY_BUFFER, Some(ebo));
-            gl.buffer_data_u8_slice(
-                glow::ELEMENT_ARRAY_BUFFER,
-                bytemuck::cast_slice(indices),
-                glow::STATIC_READ,
-            );
-
-            let loc = gl
-                .get_attrib_location(program, "vert_local_position")
-                .ok_or_else(|| "No vert_local_position attribute".to_string())?;
-            gl.enable_vertex_attrib_array(loc);
-            gl.vertex_attrib_pointer_f32(
-                loc,
-                3,
-                glow::FLOAT,
-                false,
-                std::mem::size_of::<Vertex>() as i32,
-                offset_of!(Vertex, local_position) as i32,
-            );
-            let loc = gl
-                .get_attrib_location(program, "vert_world_position")
-                .ok_or_else(|| "No vert_world_position attribute".to_string())?;
-            gl.enable_vertex_attrib_array(loc);
-            gl.vertex_attrib_pointer_f32(
-                loc,
-                3,
-                glow::FLOAT,
-                false,
-                std::mem::size_of::<Vertex>() as i32,
-                offset_of!(Vertex, world_position) as i32,
-            );
-            let loc = gl
-                .get_attrib_location(program, "vert_scale")
-                .ok_or_else(|| "No vert_scale attribute".to_string())?;
-            gl.enable_vertex_attrib_array(loc);
-            gl.vertex_attrib_pointer_f32(
-                loc,
-                3,
-                glow::FLOAT,
-                false,
-                std::mem::size_of::<Vertex>() as i32,
-                offset_of!(Vertex, scale) as i32,
-            );
-
-            let color_location = gl
-                .get_attrib_location(program, "vert_color")
-                .ok_or_else(|| "No vert_color attribute".to_string())?;
-            gl.enable_vertex_attrib_array(color_location);
-            gl.vertex_attrib_pointer_f32(
-                color_location,
-                4,
-                glow::FLOAT,
-                false,
-                std::mem::size_of::<Vertex>() as i32,
-                offset_of!(Vertex, color) as i32,
-            );
-
-            let model_matrix_location = gl
-                .get_uniform_location(program, "model")
-                .ok_or_else(|| "No model matrix attribute".to_string())?;
-            let lorentz_matrix_location = gl
-                .get_uniform_location(program, "lorentz")
-                .ok_or_else(|| "No lorentz matrix attribute".to_string())?;
-            let view_perspective_location = gl
-                .get_uniform_location(program, "view_perspective")
-                .ok_or_else(|| "No view_perspective matrix attribute".to_string())?;
-
-            Ok(Self {
-                gl,
-                vbo,
-                ebo,
-                model_matrix_location,
-                lorentz_matrix_location,
-                view_perspective_location,
-                triangle_count: indices.len(),
-            })
-        }
-    }
-
-    pub fn draw(
-        &self,
-        vertices: &[Vertex],
-        model: Matrix,
-        lorentz: Matrix,
-        view_perspective: Matrix,
-    ) -> Result<(), String> {
-        unsafe {
-            self.gl.bind_buffer(glow::ARRAY_BUFFER, Some(self.vbo));
-            self.gl.buffer_data_u8_slice(
-                glow::ARRAY_BUFFER,
-                bytemuck::cast_slice(vertices),
-                glow::DYNAMIC_DRAW,
-            );
-            self.gl
-                .bind_buffer(glow::ELEMENT_ARRAY_BUFFER, Some(self.ebo));
-
-            self.gl.uniform_matrix_4_f32_slice(
-                Some(&self.model_matrix_location),
-                false,
-                &model.open_gl(),
-            );
-            self.gl.uniform_matrix_4_f32_slice(
-                Some(&self.lorentz_matrix_location),
-                false,
-                &lorentz.open_gl(),
-            );
-            self.gl.uniform_matrix_4_f32_slice(
-                Some(&self.view_perspective_location),
-                false,
-                &view_perspective.open_gl(),
-            );
-            self.gl
-                .clear(glow::COLOR_BUFFER_BIT | glow::DEPTH_BUFFER_BIT);
-            self.gl.draw_elements(
-                glow::TRIANGLES,
-                (self.triangle_count * 3) as i32,
-                glow::UNSIGNED_INT,
-                0,
-            );
-            self.gl.flush();
-        }
-        Ok(())
-    }
 }
 
 #[wasm_bindgen]
@@ -303,13 +124,7 @@ impl App {
         let dt = (timestamp - last_tick.unwrap_or(timestamp)) / 1000.0;
         self.player.tick(dt, &self.key_manager);
 
-        let (width, height) = unsafe {
-            let mut buf = [0; 4];
-            self.backend
-                .gl
-                .get_parameter_i32_slice(glow::VIEWPORT, &mut buf);
-            (buf[2] - buf[0], buf[3] - buf[1])
-        };
+        let (width, height) = self.backend.get_viewport_size();
         let projection_matrix =
             Matrix::perspective(Deg(60.0), width as f64 / height as f64, 0.1, 10000.0);
         let rot_matrix = self.player.rot_matrix();
