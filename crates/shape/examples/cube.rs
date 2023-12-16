7de2e223cfc6747673de908f43f8cc4c16e87992
Nakayama Daiju <42.daiju@gmail.com>
2023-12-16 09:35:31 +0900

    cube to obje (debug)

---
+++ crates/shape/examples/cube.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let cube = shape::CubeOption::new()
+        .size(2.0)
+        .center([0.0, 0.0, 1.0])
+        .build();
+    let mut buf = Vec::new();
+    cube.write_as_obj(&mut buf).unwrap();
+    println!("{}", String::from_utf8(buf).unwrap());
+}
--- crates/shape/src/lib.rs
+++ crates/shape/src/lib.rs
@@ -7,3 +7,16 @@ pub struct Data {
     pub vertices: Vec<[f32; 3]>,
     pub indices: Vec<[u32; 3]>,
 }
+
+impl Data {
+    #[cfg(feature = "obj")]
+    pub fn write_as_obj<W: std::io::Write>(&self, w: &mut W) -> std::io::Result<()> {
+        for [x, y, z] in self.vertices.iter() {
+            write!(w, "v {} {} {}\n", x, y, z)?
+        }
+        for &[x, y, z] in self.indices.iter() {
+            write!(w, "f {}// {}// {}//\n", x + 1, y + 1, z + 1)?
+        }
+        Ok(())
+    }
+}
