bf18f90dfb5ed6d7d0cf785ddee3edabf589ddba
Nakayama Daiju <42.daiju@gmail.com>
2024-01-02 09:59:13 +0900

    lightingの修正

--- src/backend/glsl/lighting_fragment_shader.glsl
+++ src/backend/glsl/lighting_fragment_shader.glsl
@@ -9,5 +9,8 @@ out vec4 frag_color;
 
 void main() {
     float diffuse = max(dot(normal, light_ray), 0.0);
-    frag_color = color * (0.5 + diffuse * 0.5);
+
+    vec3 light_color = vec3(1.0, 1.0, 1.0);
+    vec3 rgb = light_color * (color.rgb * diffuse * 0.75 + 0.25);
+    frag_color = vec4(rgb, color.a);
 }
